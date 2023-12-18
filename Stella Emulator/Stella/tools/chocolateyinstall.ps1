68e11b8871e94e01d9770f9e8a94fcae6d7333ce
Oisin Hickey <39494199+ironic833@users.noreply.github.com>
2023-12-18 21:23:46 +0000

    Update chocolateyinstall.ps1
    - Problem doesn't appear to be the flags. for some reason package is stalling after identifying dependencies

--- Stella Emulator/Stella/tools/chocolateyinstall.ps1
+++ Stella Emulator/Stella/tools/chocolateyinstall.ps1
@@ -12,7 +12,7 @@ $packageArgs = @{
   softwareName  = 'Stella*' #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
 
   
-  silentArgs    = "/VERYSILENT /qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`"" # ALLUSERS=1 DISABLEDESKTOPSHORTCUT=1 ADDDESKTOPICON=0 ADDSTARTMENU=0
+  silentArgs    = "/VERYSILENT /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`"" # ALLUSERS=1 DISABLEDESKTOPSHORTCUT=1 ADDDESKTOPICON=0 ADDSTARTMENU=0
   validExitCodes= @(0, 3010, 1641)
   
 }
